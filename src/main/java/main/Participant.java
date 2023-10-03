d042d6bc2e4892035ccb6a06fab4d57a3402aca1
Benjamin Flock <bcflock@gmail.com>
2023-10-02 21:12:48 -0400

    Update Participant.java

--- src/main/java/main/Participant.java
+++ src/main/java/main/Participant.java
@@ -1,4 +1,13 @@
 package main.java.main;
 
-public record Participant {
+import java.util.UUID;
+
+public record Participant(UUID uuid, UUID eventId, String name, String email) {
+    public static Participant create(String uuid, String eventId, String name, String email) throws Event.HandledIllegalValueException {
+            return new Participant(UUID.fromString(uuid), UUID.fromString(eventId), name, Event.validateEmail(email));
+    }
+    public static Participant create(String eventId, String name, String email) throws Event.HandledIllegalValueException {
+        return Participant.create(UUID.randomUUID().toString(), eventId, name, email);
+    }
+
 }
