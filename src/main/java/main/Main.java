231093f3e0acf5acca6cfed65de7db7ca3ccb238
Andrew Megeath <avm33@case.edu>
2023-10-03 13:09:35 -0400

    finished commands

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,10 @@ import picocli.CommandLine.Parameters;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,6 +25,8 @@ public class Main {
     private static final Pattern DATE_PATTERN = Pattern.compile("^([0-9]{4})-([0-9]{2})-([0-9]{2})$");
     private static final Pattern TIME_PATTERN = Pattern.compile("^([0-9]{2}):([0-9]{2})$");
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^\\w+@\\w+\\.\\w+$");
+
+    private static DbClient dbClient;
 
     private static LocalDateTime getLocalDateTime(String date, String time, String ampm){
         Matcher dateMatcher = DATE_PATTERN.matcher(date);
@@ -56,22 +61,26 @@ public class Main {
         return LocalDateTime.of(year, month, dayOfMonth, hour, minute);
     }
 
-    private static boolean validateLength(String text, String name, int maxLength){
+    private static boolean invalidLength(String text, String name, int maxLength){
         if(text.length() <= maxLength){
-            return true;
+            return false;
         }else{
             System.out.printf("Error: %s cannot exceed %d characters\n", name, maxLength);
-            return false;
+            return true;
         }
     }
 
-    private static boolean validateEmail(String email){
+    private static boolean invalidEmail(String email){
         if(EMAIL_PATTERN.matcher(email).matches()){
-            return true;
+            return false;
         }else{
             System.out.println("Error: invalid email address");
-            return false;
+            return true;
         }
+    }
+
+    private static UUID getOrCreateUUID(String uuidValue){
+        return uuidValue == null || uuidValue.isEmpty() ? UUID.randomUUID() : UUID.fromString(uuidValue);
     }
 
     @Command(name = "event", mixinStandardHelpOptions = true)
@@ -95,27 +104,108 @@ public class Main {
         @Parameters(index = "5")
         String hostEmail;
 
-        @Option(names = {"-u", "--uuid"})
-        String uuidValue;
+        @Option(names = {"-ei", "--event-id"})
+        String eventID;
 
         @Override
-        public Integer call(){
+        public Integer call() throws SQLException {
             LocalDateTime dateTime = getLocalDateTime(date, time, ampm);
             if(dateTime == null){
                 return 1;
             }
 
-            if(!validateLength(title, "title", EVENT_TITLE_MAX_LENGTH)
-                    || !validateLength(description, "description", EVENT_DESC_MAX_LENGTH)
-                    || !validateEmail(hostEmail)){
+            if(invalidLength(title, "title", EVENT_TITLE_MAX_LENGTH)
+                    || invalidLength(description, "description", EVENT_DESC_MAX_LENGTH)
+                    || invalidEmail(hostEmail)){
                 return 1;
             }
 
+            dbClient.addEvent(new Event(getOrCreateUUID(eventID), dateTime, title, description, hostEmail));
             return 0;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    @Command(name = "participant", mixinStandardHelpOptions = true)
+    private static class ParticipantCommand implements Callable<Integer> {
+
+        @Parameters(index = "0")
+        String eventID;
+
+        @Parameters(index = "1")
+        String name;
+
+        @Parameters(index = "2")
+        String email;
+
+        @Option(names = {"-pi", "--participant-id"})
+        String participantID;
+
+        @Override
+        public Integer call() throws SQLException {
+            if(invalidLength(name, "name", PARTICIPANT_NAME_MAX_LENGTH) || invalidEmail(email)){
+                return 1;
+            }
+
+            dbClient.addParticipant(
+                    new Participant(getOrCreateUUID(participantID), UUID.fromString(eventID), name, email)
+            );
+            return 0;
+        }
+
+    }
+
+    @Command(name = "list-events", mixinStandardHelpOptions = true)
+    private static class ListEventsCommand implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws SQLException, Event.HandledIllegalValueException {
+            List<Event> events = dbClient.getEvents();
+            StringBuilder sb = new StringBuilder();
+
+            for(Event event : events){
+                sb.append(String.format(
+                        "%s on %d-%d-%d at %d:%d | Host Email: %s | Event ID: %s | '%s' \n\n",
+                        event.title(),
+                        event.eventDateTime().getYear(), event.eventDateTime().getMonthValue(),
+                        event.eventDateTime().getDayOfMonth(),
+                        event.eventDateTime().getHour(), event.eventDateTime().getMinute(),
+                        event.hEmail(), event.uuid(), event.description()
+                ));
+            }
+
+            System.out.println(sb);
+            return 0;
+        }
+
+    }
+
+    @Command(name = "list-participants", mixinStandardHelpOptions = true)
+    private static class ListParticipantsCommand implements Callable<Integer> {
+
+        @Parameters(index = "0")
+        String eventID;
+
+        @Override
+        public Integer call() throws SQLException, Event.HandledIllegalValueException {
+            List<Participant> participants = dbClient.getParticipants(eventID);
+            StringBuilder sb = new StringBuilder();
+
+            for(Participant participant : participants){
+                sb.append(String.format(
+                        "%s | Email : %s | Participant ID: %s",
+                        participant.name(),participant.email(), participant.uuid()
+                ));
+            }
+
+            System.out.println(sb);
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        dbClient = new DbClient();
+
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line;
 
@@ -125,11 +215,11 @@ public class Main {
             System.arraycopy(words, 1, commandArgs, 0, commandArgs.length);
 
             switch(words[0]){
-                case "event":
-                    new CommandLine(new EventCommand()).execute(commandArgs);
-                    break;
-                default:
-                    System.out.println("Error: unknown command");
+                case "event" -> new CommandLine(new EventCommand()).execute(commandArgs);
+                case "participant" -> new CommandLine(new ParticipantCommand()).execute(commandArgs);
+                case "list-events" -> new CommandLine(new ListEventsCommand()).execute(commandArgs);
+                case "list-participants" -> new CommandLine(new ListParticipantsCommand()).execute(commandArgs);
+                default -> System.out.println("Error: unknown command");
             }
         }
     }
