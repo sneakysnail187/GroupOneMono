f1d051efe808af7a8a3ee8ffc8541fcfcbf9f141
Benjamin Flock <bcflock@gmail.com>
2023-10-02 21:54:04 -0400

    Updated path to package to be Maven conformant

--- src/main/java/main/Event.java
+++ src/main/java/main/Event.java
@@ -1,4 +1,4 @@
-package main.java.main;
+package main;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
--- src/main/java/main/Participant.java
+++ src/main/java/main/Participant.java
@@ -1,4 +1,4 @@
-package main.java.main;
+package main;
 
 import java.util.UUID;
 
